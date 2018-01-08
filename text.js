b7b6070a56e7df98479fecdbf4a70dffb8837acc
Dukeboxz <u04sjj@gmail.com>
2018-01-08 20:13:24 +0000

    Text file for website

---
+++ text.js
@@ -0,0 +1,25 @@
+var websiteText = {
+  volunteerPage = {
+    healthAndFitness = {
+    topText: "Our garden can make you fit and healthy";
+    middleText:  "From chopping wood to digging plots you get a full body work out at the garden";
+    bottomText: "plotting and planting while less demanding still gives a workout"
+  },
+  learnNewSkills = {
+    topText: "Being a volunteer at the garden is a great way to learn new skills from other volunteers or teach some yourself";
+    middleText: "The garden holds a number of workshops throughout the year on a range of topics from beginners guide to planting a vegetable garden to installing solar power for your garden shed";
+    bottomText: "So come and learn something new today";
+  },
+  getSocial = {
+    topText: "It is not all about work here at the garden.  We are also a place for people to relax and meet people";
+    middleText:  "Throughout the year we hodl open day and volunteer events to allow volunteers and visitors to see what we are about";
+    bottomText:  "we welcome families as well and have family themed events during the year such as our Tree Dressing day";
+  },
+  generalText = {
+    volunteerFaq: "If you still have questions about becoming a volunteer go to our volunteer FAQ page";
+    contactUs: "Interested in volunteering just come down to the garden or contact us via social media or by filling in the form here";
+    video: "Check out this video to see how the garden has benefitted some of our volunteers"; 
+  }
+
+  }
+}
