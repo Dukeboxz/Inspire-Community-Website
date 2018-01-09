96848e01b5b9ad095d30c10af2e9a7eaa5e76914
Dukeboxz <u04sjj@gmail.com>
2018-01-09 20:59:08 +0000

    Further Development to volunteer js for page

--- volunteer.js
+++ volunteer.js
@@ -1,15 +1,26 @@
 
 function loadScheme(scheme){
-
+  var healthButton = document.getElementById("getFitButton");
+  var learnSkillButton = document.getElementById("skillsButton");
+  var beSocialButton = document.getElementById("newPeopleButton");
   switch (scheme) {
     case 1:
       loadHealthScene();
+      healthButton.style.backgroundColor = "#4CAF50";
+      learnSkillButton.style.backgroundColor = "#ecffb3";
+      beSocialButton.style.backgroundColor = "#ecffb3";
       break;
     case 2:
       loadSkillsScene();
+      healthButton.style.backgroundColor = "#ecffb3" ;
+      learnSkillButton.style.backgroundColor = "#4CAF50";
+      beSocialButton.style.backgroundColor = "#ecffb3";
       break;
     case 3:
     loadSocialScene();
+    healthButton.style.backgroundColor = "ecffb3";
+    learnSkillButton.style.backgroundColor = "#ecffb3";
+    beSocialButton.style.backgroundColor = "#4CAF50";
     break;
 
     default:
@@ -22,25 +33,48 @@ function loadScheme(scheme){
 
 function loadHealthScene(){
   console.log("load Health Scheme");
-  console.log(websiteText.learnNewSkills.topText); 
+  console.log(websiteText.learnNewSkills.topText);
   var panel = document.getElementById("schmeDiv");
 
   while(panel.firstChild) {
     panel.removeChild(panel.firstChild);
   }
 
- var womanPathImage = document.createElement("img");
- womanPathImage.className = "elementImage";
- womanPathImage.src= "images/woman_clearing_Path.jpg"
- womanPathImage.id= "womanPathImage";
 
- var healthSchemeText = document.createElement("P");
+ var healthSchemeTopText = document.createElement("div");
 
- healthSchemeText.innerHTML= "Our garden can make you fit and healthy "
- healthSchemeText.id="healthSchemeText";
+healthSchemeTopText.innerHTML= websiteText.healthAndFitness.topText;
+ healthSchemeTopText.className="topText";
+ var topImage = document.createElement("img");
+ topImage.className = "topImage";
+ topImage.src= "images/Women_clearing_Path.jpg"
+ topImage.id= "womanPathImage";
 
- panel.appendChild(womanPathImage);
- panel.appendChild(healthSchemeText)
+
+
+ var healthSchemeMiddleText = document.createElement("div");
+ healthSchemeMiddleText.innerHTML = websiteText.healthAndFitness.middleText;
+ healthSchemeMiddleText.className="middleText";
+ var middleImage = document.createElement("img");
+ middleImage.className = "middleImage";
+ middleImage.src = "images/Orchard_Planting.jpg";
+
+var healthSchemeBottomText = document.createElement("div");
+healthSchemeBottomText.innerHTML = websiteText.healthAndFitness.bottomText;
+healthSchemeBottomText.className= "bottomText";
+var bottomImage = document.createElement("img");
+bottomImage.className="bottomImage";
+bottomImage.src="images/workInPlottingShed.jpg";
+
+
+
+ panel.appendChild(healthSchemeTopText);
+ panel.appendChild(topImage);
+ panel.appendChild(healthSchemeMiddleText);
+ panel.appendChild(middleImage);
+ panel.appendChild(healthSchemeBottomText);
+ panel.appendChild(bottomImage);
+
 
 
 }
