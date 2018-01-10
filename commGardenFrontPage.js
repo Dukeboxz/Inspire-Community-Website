c1b43c7643e1fca71d0ac06007cb5f9d0367d409
Dukeboxz <u04sjj@gmail.com>
2018-01-10 21:15:30 +0000

    Start of functions to display blogs on site

--- commGardenFrontPage.js
+++ commGardenFrontPage.js
@@ -9,8 +9,33 @@ window.onclick = function(event){
     for(i = 0; i < dropdowns.length; i++){
       var openDropdown = dropdowns[i];
       if(openDropdown.classList.contains('show')){
-        openDropdown.classList.remove('show'); 
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
+
+function getBlogPosts() {
+  console.log("getblog post");
+  var xmlhttp = new XMLHttpRequest();
+  var url = "https://www.googleapis.com/blogger/v3/blogs/16981595?key=AIzaSyBaJW75-VpKaN0V1-Ttyit90i3o9FUEYT8";
+
+  xmlhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200){
+      var response = this.responseText;
+      console.log(response);
+      //displayPosts(response);
+    }
+  }
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+function displayPosts(jsonReponse){
+  //var out = "";
+  //for(var i;  i < 4; i++){
+  //  out += jsonReponse.items[i].content;
+  //}
+  var blog = document.getElementsByClassName("blogDiv")[0];
+    blog.innerHTML = jsonResponse;
 }
