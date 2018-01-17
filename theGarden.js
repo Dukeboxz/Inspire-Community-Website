8f3adeb5544c5f7db28ac98495961519cb7ea210
Dukeboxz <u04sjj@gmail.com>
2018-01-17 21:02:59 +0000

    Added clickable image to page

--- theGarden.js
+++ theGarden.js
@@ -1,3 +1,20 @@
 function launchPopUp(plotNumber){
-  alert("plot " + plotNumber + " has been clicked")
+
+  var newWindow = window.open("", plotNumber, "menubar=no, width=200,height=200");
+
+  var windowTitle = document.createElement("div");
+  windowTitle.innerHTML = "Plot Number " + plotNumber;
+
+  newWindow.document.body.appendChild(windowTitle);
+  var cropPlanted = document.createElement("div");
+  cropPlanted.innerHTML="sweetcorn";
+
+  var wikipediaEntry = document.createElement("iframe");
+  wikipediaEntry.className = "wikiEntry";
+  wikipediaEntry.src = "https://en.wikipedia.org/wiki/" + cropPlanted.innerHTML;
+
+  newWindow.document.body.appendChild(cropPlanted);
+  newWindow.document.body.appendChild(wikipediaEntry);
+
+
 }
